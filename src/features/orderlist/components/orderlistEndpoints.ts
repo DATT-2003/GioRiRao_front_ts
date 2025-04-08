7dbf1ea0d0a3437aa3130212d86d9583232ddcfb
whiskeeyy <kietduongdinh@gmail.com>
2025-04-08 10:25:26 +0700

    Revert "Done Order checklist implement Order Checklist page"
    This reverts commit 023976db8d8aeb9a443677097438d87b02bd5164.

--- src/features/orderlist/components/orderlistEndpoints.ts
+++
@@ -1,6 +0,0 @@
-const orderlistEndpoints = {
-  getPendingOrders: (storeId: string) => `/orders/pending-orders/${storeId}`,
-  updateOrderStatus: (orderId: string) => `/orders/${orderId}`,
-};
-
-export default orderlistEndpoints;
