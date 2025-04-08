59a0f394f9e61e04b75d0303478b4343f41d5145
whiskeeyy <kietduongdinh@gmail.com>
2025-04-08 10:25:16 +0700

    Revert "Update orderlistTypes.ts"
    This reverts commit b456a8b39e07b2e41d76ba37f924543cacac2537.

--- src/features/orderlist/components/orderlistTypes.ts
+++ src/features/orderlist/components/orderlistTypes.ts
@@ -1,22 +1,8 @@
-export interface IOrderItem {
-  drinkId: string;
-  drinkName: string;
-  quantity: number;
-  note?: string;
-  size: string;
-  toppings: {
-    toppingId: string;
-    name: string;
-    quantity: number;
-  }[];
-}
-
 export interface IOrder {
   _id: string;
   code: string;
-  createdBy: string;
   storeId: string;
-  items: IOrderItem[]; // ✅ chính là dòng bị thiếu
+  createdBy: string;
   status: "PENDING" | "COMPLETED";
   paymentMethod: string;
   total: number;
